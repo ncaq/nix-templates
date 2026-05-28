fd57edd23449e95bd9b6774de13dcf50a127ada7
ncaq <ncaq@ncaq.net>
2026-05-28 11:28:16 +0900

    feat(typescript): Viteによるバンドル前提のプロジェクト構成に変更
    結局バンドラを使うことが多いので、
    バンドラなしの前提で作っていたテンプレート構成を、
    Viteによるバンドル前提の構成に変更しました。
    
    バックエンドかフロントエンドかまではまだ細分化していないので、
    そのへんはプロジェクト側で調整することにします。
    
    バンドラなしの`Node.js`によるTypeScript直接実行などに切り替える場合も、
    少しViteを消すぐらいで結局vitestなどは使うことになると思うので、
    そこまで大変な作業にはならないはずです。

--- templates/typescript/vite.config.ts
+++ templates/typescript/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 const config: ReturnType<typeof defineConfig> = defineConfig({
+  build: {
+    outDir: "dist",
+  },
   test: {
     include: ["test/**/*.test.ts"],
+    exclude: [...configDefaults.exclude, "**/.direnv/**", "**/dist/**"],
   },
 });
 export default config;
