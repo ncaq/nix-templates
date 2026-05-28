b83bde2512c07074a01a0bbf0e128cf2928c38fe
ncaq <ncaq@ncaq.net>
2026-05-28 11:53:09 +0900

    fix(typescript): 便宜上のライブラリエントリポイントを追加してビルドチェック追加
    - `vite.config.ts`でlib出力形式をESに限定しエントリポイントを指定
    - `flake.nix`にbuildチェックを追加

--- templates/typescript/vite.config.ts
+++ templates/typescript/vite.config.ts
@@ -3,6 +3,10 @@ import { configDefaults, defineConfig } from "vitest/config";
 const config: ReturnType<typeof defineConfig> = defineConfig({
   build: {
     outDir: "dist",
+    lib: {
+      formats: ["es"],
+      entry: ["src/index.ts"],
+    },
   },
   test: {
     include: ["test/**/*.test.ts"],
